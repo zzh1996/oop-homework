f5ea93518635741d77bdf195a29c102549746eda
zzh1996 <zzh1996@mail.ustc.edu.cn>
2016-12-08 23:29:21 +0800

    add tests for Exp3

--- Exp3/library.cpp
+++ Exp3/library.cpp
@@ -110,5 +110,68 @@ int main(){
     }catch(exception &e){
         cout<<e.what()<<endl;
     }
+    cout<<b.get_ISBN()<<endl;
+    cout<<b.get_name()<<endl;
+    cout<<b.get_date()<<endl;
+    cout<<b.is_lent()<<endl;
+    b.borrow();
+    cout<<b.is_lent()<<endl;
+    try{
+        b.borrow();
+    }catch(exception &e){
+        cout<<e.what()<<endl;
+    }
+    b.return_();
+    cout<<b.is_lent()<<endl;
+    try{
+        b.return_();
+    }catch(exception &e){
+        cout<<e.what()<<endl;
+    }
+
+    Patron p("Tom","A123456");
+    cout<<p.get_name()<<endl;
+    cout<<p.get_ID()<<endl;
+    cout<<p.has_arrear()<<endl;
+    p.set_fee(12.34);
+    cout<<p.has_arrear()<<endl;
+    cout<<p.get_fee()<<endl;
+
+    Library l;
+    l.add_book(Book("12-34-56-A","book1","author1",Date(2016,Date::jan,1)));
+    l.add_book(Book("12-34-78-B","book2","author2",Date(2016,Date::jan,2)));
+    l.add_book(Book("12-34-90-C","book3","author3",Date(2016,Date::jan,3)));
+    l.add_patron(Patron("Alice","123456"));
+    Patron Bob("Bob","123457");
+    Bob.set_fee(1.2);
+    l.add_patron(Bob);
+    l.add_patron(Patron("CCC","123458"));
+    Patron DDD("DDD","123459");
+    DDD.set_fee(3.4);
+    l.add_patron(DDD);
+    l.borrow_book("123456","12-34-56-A");
+    try{
+        l.borrow_book("123458","12-34-56-A");
+    }catch(exception &e){
+        cout<<e.what()<<endl;
+    }
+    try{
+        l.borrow_book("123457","12-34-78-B");
+    }catch(exception &e){
+        cout<<e.what()<<endl;
+    }
+    try{
+        l.borrow_book("123458","12-34-00-0");
+    }catch(exception &e){
+        cout<<e.what()<<endl;
+    }
+    try{
+        l.borrow_book("123","12-34-78-B");
+    }catch(exception &e){
+        cout<<e.what()<<endl;
+    }
+    for(string name : l.patrons_with_arrear()){
+        cout<<name<<endl;
+    }
     return 0;
 }
