65535454a934f8c4f3cd4c212d09308aa22672ab
zzh1996 <zzh1996@mail.ustc.edu.cn>
2016-12-08 17:32:41 +0800

    Exp3 other classes

--- Exp3/library.cpp
+++ Exp3/library.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <exception>
 #include <regex>
 #include "Chrono.h"
 
@@ -20,7 +19,7 @@ class Book{
     Date date;
     bool lent;
 public:
-    Book(string ISBN,string name,string author,Date date):
+    Book(const string &ISBN,const string &name,const string &author,const Date &date):
         ISBN(ISBN),name(name),author(author),date(date),lent(false){
             regex re("[0-9]+-[0-9]+-[0-9]+-[0-9A-Z]");
             if(!regex_match(ISBN,re))
@@ -28,7 +27,7 @@ public:
         }
     void borrow(){
         if(lent)
-            throw LibraryError("Book already lent");
+            throw LibraryError("Book is already lent");
         lent=true;
     }
     void return_(){
@@ -42,9 +41,74 @@ public:
     bool is_lent() const {return lent;}
 };
 
+class Patron{
+    string name;
+    string ID;
+    double fee;
+public:
+    Patron(const string &name,const string &ID):name(name),ID(ID),fee(0){}
+    string get_name() const {return name;}
+    string get_ID() const {return ID;}
+    double get_fee() const {return fee;}
+    void set_fee(const double f){fee=f;}
+    bool has_arrear() const {return fee>0;}
+};
+
+struct Transaction{
+    Book book;
+    Patron patron;
+    Date date;
+};
+
+class Library{
+public:
+    vector<Book> books;
+    vector<Patron> patrons;
+    vector<Transaction> transactions;
+    Book &get_book_by_ISBN(const string &ISBN){
+        for(Book &b : books){
+            if(b.get_ISBN()==ISBN)
+                return b;
+        }
+        throw LibraryError("ISBN not found");
+    }
+    Patron &get_patron_by_ID(const string &ID){
+        for(Patron &p : patrons){
+            if(p.get_ID()==ID)
+                return p;
+        }
+        throw LibraryError("ID not found");
+    }
+    void add_book(const Book &book){books.push_back(book);}
+    void add_patron(const Patron &patron){patrons.push_back(patron);}
+    void borrow_book(const string pID,const string ISBN){
+        Patron &p=get_patron_by_ID(pID);
+        Book &b=get_book_by_ISBN(ISBN);
+        if(p.has_arrear())
+            throw LibraryError("Patron has arrear");
+        if(b.is_lent())
+            throw LibraryError("Book is lent");
+        transactions.push_back(Transaction{b,p,Date()});
+        b.borrow();
+    }
+
+    vector<string> patrons_with_arrear(){
+        vector<string> r;
+        for(Patron &p : patrons){
+            if(p.has_arrear())
+                r.push_back(p.get_name());
+        }
+        return r;
+    }
+};
+
 
 int main(){
     Book b("1-2-3-X","name1","author1",Date(2016,Date::jan,1));
-    Book b2("1-2-A-X","name1","author1",Date(2016,Date::jan,1));
+    try{
+        Book b2("1-2-A-X","name1","author1",Date(2016,Date::jan,1));
+    }catch(exception &e){
+        cout<<e.what()<<endl;
+    }
     return 0;
 }
